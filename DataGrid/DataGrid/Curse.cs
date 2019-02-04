b88a14ba3ef3f890503f9bb54bff96fba3dc027c
BarakpaevOlzhas <40425093+BarakpaevOlzhas@users.noreply.github.com>
2019-02-04 20:00:56 +0600

    Add files via upload

---
+++ DataGrid/DataGrid/Curse.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace DataGrid
+{
+    public class Curse
+    {
+        public string Id { get; set; }
+        public string Name_Kaz { get; set; }
+        public string Edinica_Izmerenia { get; set; }
+        public string Sootnowenie { get; set; }
+        public string Name_Rus { get; set; }
+        public string Kurs { get; set; }
+        public string Kod { get; set; }
+    }
+}
+
---
+++ DataGrid/DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            InitializeComponent();
+            WebRequest request = WebRequest.Create("https://data.egov.kz/api/v2/valutalar_bagamdary4/v501?pretty");
+            HttpWebResponse resp = (HttpWebResponse)request.GetResponse();
+
+            List<Curse> data = new List<Curse>();
+
+            
+            using (StreamReader stream = new StreamReader(resp.GetResponseStream(), Encoding.UTF8))
+            {
+                string JsonFormat = stream.ReadToEnd();
+                data = JsonConvert.DeserializeObject<List<Curse>>(JsonFormat);
+            }
+
+            foreach (var i in data)
+            {
+                var Shop = new Curse
+                {
+                    Sootnowenie = i.Sootnowenie,
+                    Name_Rus = "KZT",
+                    Kurs = i.Kurs,
+                    Kod = i.Kod
+                };
+
+                dataG.Items.Add(Shop);
+            }           
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
